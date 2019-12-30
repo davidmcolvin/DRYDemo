ffc25a420e1919922f2ceab630ad5c2f26d3d8dc
davidcol1 <davidmcolvin@gmail.com>
2019-12-29 20:02:57 -0500

    Change how we generate EmployeeID's and add unit tests

--- DRYDemoLibrary/EmployeeProcessor.cs
+++ DRYDemoLibrary/EmployeeProcessor.cs
@@ -10,8 +10,27 @@ namespace DRYDemoLibrary
   {
     public string GenerateEmployeeID(string firstName, string lastName)
     {
-      string employeeId = $@"{firstName.Substring(0, 4)}{lastName.Substring(0, 4)}{DateTime.Now.Millisecond}";
+      string employeeId = $@"{GetPartOfName(firstName, 4)}{GetPartOfName(lastName, 4)}{DateTime.Now.Millisecond}";
       return employeeId;
+    }
+
+    private string GetPartOfName(string name, int numberOfCharacters)
+    {
+      string output = name;
+
+      if (name.Length >= numberOfCharacters)
+      {
+        output = name.Substring(0, numberOfCharacters);
+      }
+      else
+      {
+        do
+        {
+          output += "X";
+        } while (output.Length < 4);
+      }
+
+      return output;
     }
   }
 }
